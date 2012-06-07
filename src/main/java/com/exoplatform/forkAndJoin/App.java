99bc8be92fc7c9eba4cb36510b81896156af28a1
Vlad Zhukovskii <me@jpro.org.ua>
2012-06-07 14:21:37 +0300

    final office version for the 08.06.12

--- src/main/java/com/exoplatform/forkAndJoin/App.java
+++ src/main/java/com/exoplatform/forkAndJoin/App.java
@@ -1,13 +1,28 @@
 package com.exoplatform.forkAndJoin;
 
+import jsr166y.RecursiveTask;
+
 /**
- * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String... args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "some test" );
+        //new ForkJoinPool().invoke(new ValueSumCounter(10));
+    }
+}
+
+class ValueSumCounter extends RecursiveTask<Integer>{
+    private Integer var;
+
+    public ValueSumCounter(Integer var) {
+        this.var = var;
+    }
+
+    @Override
+    protected Integer compute() {
+        return var;
     }
 }
