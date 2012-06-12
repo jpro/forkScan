6faa7446580b57576853ebea4cb171dff3c3dd77
Vlad Zhukovskii <me@jpro.org.ua>
2012-06-12 15:07:28 +0300

    added comments

--- src/main/java/com/exoplatform/forkScan/Scan.java
+++ src/main/java/com/exoplatform/forkScan/Scan.java
@@ -1,5 +1,16 @@
 package com.exoplatform.forkScan;
 
+/**
+ * Interface that define contract which classes that implements this interface must be implement method getStat.
+ * This method retrieve statistic for algorithm of calculation files size and files/directories count.
+ */
 public interface Scan {
+
+    /**
+     * Retrieve statistic of work algorithm
+     * @param path - path which must be viewed
+     * @param threadCount - threads count //TODO implement in classes with constant
+     * @return - object of statistic
+     */
     public Statistic getStat(String path, int threadCount);
 }
