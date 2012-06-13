1d26f80cf1eb3ee370cd923632a050b63748ddda
Vlad Zhukovskii <me@jpro.org.ua>
2012-06-13 15:54:52 +0300

    added instruction that create run.sh in target directory

--- compile.sh
+++ compile.sh
@@ -3,3 +3,8 @@ mvn clean install
 cd target
 ls -l | awk -F'[0-9][0-9]:[0-9][0-9]' '/^d/{print $NF}'| xargs -i rm -rf '{}' \;
 mv fork*.jar forkScan.jar
+echo "#!/bin/bash" > run.sh
+echo "java -jar forkScan.jar /usr ScanRecursive" >> run.sh
+echo "java -jar forkScan.jar /usr ScanThread 2" >> run.sh
+echo "java -jar forkScan.jar /usr ScanOptimize 2" >> run.sh
+chmod +x run.sh
